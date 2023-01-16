a5d7df6fc19558dd06300632940b9a0a87a19b92
Hrvoje Lukšić <hluksic20@foi.hr>
2023-01-16 16:15:36 +0100

    Izrađene forme za login i registraciju, logika TODO #16

---
+++ Software/StoryWatch/StoryWatch/Registracija.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace StoryWatch
+{
+    /// <summary>
+    /// Interaction logic for Registracija.xaml
+    /// </summary>
+    public partial class Registracija : Window
+    {
+        private readonly Window parent = null;
+
+        public Registracija(Window parent)
+        {
+            InitializeComponent();
+            this.parent = parent;
+        }
+
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+            parent.Show();
+        }
+    }
+}
