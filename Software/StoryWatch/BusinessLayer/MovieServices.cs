aa768c490410171d90a974b94c6e98decee4f1e7
Noa Midzic <nmidzic@yahoo.com>
2023-01-18 12:05:42 +0100

    Added test method GetMovieTMDB, need to add API key

---
+++ Software/StoryWatch/BusinessLayer/MovieServices.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMDbLib.Client;
+
+namespace BusinessLayer
+{
+    public class MovieServices
+    {
+        public void GetMovieTMDB()
+        {
+            TMDbClient client = new TMDbClient("APIKey");
+            TMDbLib.Objects.Movies.Movie movie = client.GetMovieAsync(47964).Result;
+
+            Console.WriteLine($"Movie name: {movie.Title}");
+        }
+
+    }
+}
