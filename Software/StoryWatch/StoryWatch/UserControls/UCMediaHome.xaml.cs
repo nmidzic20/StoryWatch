20c5c417d65c12a544bcd0abd5b7bf39451e87fc
Noa Midzic <nmidzic@yahoo.com>
2023-01-18 17:03:10 +0100

    Removed unused code

--- Software/StoryWatch/StoryWatch/UserControls/UCMediaHome.xaml.cs
+++ Software/StoryWatch/StoryWatch/UserControls/UCMediaHome.xaml.cs
@@ -41,29 +41,14 @@ namespace StoryWatch.UserControls
 
             foreach (var lc in listCategories)
             {
-                //ListBox lb = CreateListBox(lc.Title, lc.Color);
-                UserControl lb = new MediaListBox(lc.Title, lc.Color);
-                ContentControl cc = new ContentControl
+                UserControl mediaListBox = new MediaListBox(lc.Title, lc.Color);
+                ContentControl control = new ContentControl
                 {
-                    Content = lb
+                    Content = mediaListBox
                 };
 
-
-                //lazy loading problem
-                //ListBoxItem itemTitle = (ListBoxItem) (lb.ItemContainerGenerator.ContainerFromIndex(0));
-                //itemTitle.Background = new SolidColorBrush(Colors.BlueViolet);
-
-                AddListBoxToGrid(cc);
+                AddListBoxToGrid(control);
             }
-
-            /*int i = 0;
-            foreach (var element in gridLists.Children.Cast<UIElement>())
-            {
-                var lb = element as ListBox;
-                ListBoxItem itemTitle = (ListBoxItem) (lb.ItemContainerGenerator.ContainerFromIndex(i));
-                itemTitle.Background = new SolidColorBrush(Colors.BlueViolet);
-                i++;
-            }*/
 
         }
 
@@ -85,41 +70,7 @@ namespace StoryWatch.UserControls
 
         private void btnAddCustomList_Click(object sender, RoutedEventArgs e)
         {
-            GuiManager.OpenContent(new UCAddCustomList());
-            //ovo je samo pokazni primjer
-
-            /*ListBox newCustomList = new ListBox();
-
-            newCustomList.Items.Add("Mon");
-            newCustomList.Items.Add("Tue");
-            newCustomList.Items.Add("Wed");
-            newCustomList.Items.Add("Thu");
-            newCustomList.Items.Add("Fri");
-
-            newCustomList.Width = 180;
-            newCustomList.Height = 200;
-            newCustomList.Name = "lbCustom"; //dati korisnički definirano ime
-            newCustomList.Margin = new Thickness(20);
-            ScrollViewer.SetVerticalScrollBarVisibility(newCustomList, ScrollBarVisibility.Visible);
-
-            AddListBoxToGrid(newCustomList);*/
-            
-        }
-
-        private ListBox CreateListBox(string content, string colorString)
-        {
-            ListBox lb = new ListBox();
-            lb.Width = 180;
-            lb.Height = 200;
-            lb.Margin = new Thickness(20);
-
-            Color color = (Color)ColorConverter.ConvertFromString(colorString);
-            lb.Background = new SolidColorBrush(color);
-
-            lb.Items.Add(content);
-
-            return lb;
-
+            GuiManager.OpenContent(new UCAddCustomList());           
         }
 
         private void AddListBoxToGrid(ContentControl list)//ListBox list)
