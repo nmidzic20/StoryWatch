7838fc99aebdab1b9471449c254a4f9075a79c9a
Hrvoje Lukšić <hluksic20@foi.hr>
2023-01-21 14:22:07 +0100

    Instaliran API za igre, implementirana forma za dodavanje igara i klasa za dohvat #24

---
+++ Software/StoryWatch/BusinessLayer/GameServices.cs
@@ -0,0 +1,30 @@
+﻿using EntitiesLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IGDB;
+using IGDB.Models;
+using System.Web.UI;
+
+namespace BusinessLayer
+{
+    public class GameServices
+    {
+        private readonly string id = "ba8wwb974gpp9d1azk47t4g7mzibg5";
+        private readonly string secret = "toc5evt7on3gbpkr6iwtolt7swobvi";
+
+        private IGDBClient api;
+
+        public GameServices()
+        {
+            api = new IGDBClient(id, secret);
+        }
+
+        public async Task<IGDB.Models.Game[]> SearchGamesAsync(string name)
+        {
+            return await api.QueryAsync<IGDB.Models.Game>(IGDBClient.Endpoints.Games, query: $"fields id,name; where id = {name};");
+        }
+    }
+}
---
+++ Software/StoryWatch/StoryWatch/UserControls/Games/UCAddGame.xaml.cs
@@ -0,0 +1,80 @@
+﻿using BusinessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StoryWatch.UserControls.Games
+{
+    /// <summary>
+    /// Interaction logic for UCAddGame.xaml
+    /// </summary>
+    public partial class UCAddGame : UserControl
+    {
+        private GameServices gameServices;
+        private readonly string delimiter = " | ID: ";
+
+        public UCAddGame()
+        {
+            gameServices = new GameServices();
+        }
+
+        private async void txtSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (lbResults == null ||
+                string.IsNullOrEmpty(txtSearchKeyword.Text)/* ||
+                txtSearchKeyword.Text == placeholderTextKeyword*/)
+
+                return;
+
+            lbResults.Items.Clear();
+
+            var games = await gameServices.SearchGamesAsync(txtSearchKeyword.Text);
+
+            if (games == null)
+            {
+                //txtResults.Text = "";
+                return;
+            }
+
+            //var text = "";
+
+            foreach (var game in games)
+                //text += r + "\n";
+                lbResults.Items.Add(game.Name + delimiter + game.Id);
+
+            //txtResults.Text = text;
+        }
+
+        //private async void lbResults_SelectionChangedAsync(object sender, SelectionChangedEventArgs e)
+        //{
+        //    if (e.AddedItems.Count == 0) return;
+
+        //    var item = e.AddedItems[0] as string;
+        //    var id = item.Split(new string[] { delimiter }, StringSplitOptions.RemoveEmptyEntries)[1];
+
+        //    int idInt;
+        //    Int32.TryParse(id, out idInt);
+
+        //    string movieInfo = "";
+        //    TMDbLib.Objects.Movies.Movie movie = await movieServices.GetMovieInfoAsync(idInt);
+
+        //    string urlYoutube = "https://www.youtube.com/watch?v=";
+        //    string trailerURL = urlYoutube + movie.Videos.Results[0].Key;
+        //    movieInfo += movie.Title + " " + movie.Homepage + " " + movie.Genres[0].Name + " "
+        //        + movie.Runtime + " " + movie.BackdropPath + " " + trailerURL;
+
+        //    MessageBox.Show("TODO add movie info into textboxes " + id + " " + movieInfo);
+        //}
+    }
+}
